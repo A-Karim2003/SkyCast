788cd81326290ec372f720a10976cba8f7d99cad
Abdoulkarim <17a.karim@gmail.com>
2025-09-13 22:45:16 +0100

    restructured and created components skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 // import { useState } from "react";
+import Background from "./components/background";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="app">
-      <img className="background" src="/src/assets/background/rainy.jpg" />
-      <main></main>
+      <Background />
+      <Main>
+        <Header />
+      </Main>
     </div>
   );
 }
---
+++ src/components/background.jsx
@@ -0,0 +1,5 @@
+function Background() {
+  return <img className="background" src="/src/assets/background/stormy.jpg" />;
+}
+
+export default Background;
