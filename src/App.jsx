a794355508b9653c38c8e068618ad19bbb4dfde5
Abdoulkarim <17a.karim@gmail.com>
2025-09-13 21:19:52 +0100

    feat: setup base styles, background image, and glassmorphism layout
    - Configured body with fullscreen background image and dark overlay
    - Introduced glassmorphism effect for <main> section:
      - backdrop-filter blur
      - semi-transparent background
      - border radius and shadows for depth

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <img className="background" src="/src/assets/background/rainy.jpg" />
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
